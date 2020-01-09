8aa89c3a9c9a9a1b743328b70bde8a8e41aad9c0
meron <malemu17@yahoo.com>
2020-01-09 00:45:44 -0800

    workenv

---
+++ socket-fetch/src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+function App() {
+  return (
+    <div>
+      <h1>Merry Christmas Mad Lib</h1>
+      <h2>Fill form fields to see The Mad Lib!</h2>
+     
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
+
